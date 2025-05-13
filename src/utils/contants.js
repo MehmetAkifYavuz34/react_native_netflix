b9bc7a8cdb64ac6cb82b8dbcddb223303c6cd0de
Mehmet Akif Yavuz <mehmetakfyavuz@gmail.com>
2025-05-13 20:51:30 +0300

    Update contants.js

--- src/utils/contants.js
+++ src/utils/contants.js
@@ -1,7 +1,7 @@
-const API_KEY = '4447049e7703aa28f5317e3d4a8e0f24';
+const API_KEY = 
 
 const token =
-  'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NDQ3MDQ5ZTc3MDNhYTI4ZjUzMTdlM2Q0YThlMGYyNCIsIm5iZiI6MTczNzU0NzAyNS44MTEsInN1YiI6IjY3OTBkZDExNTUwZTZmYzNjZGRmZmI5OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fjBXBnIOPDy0pmT0wx8jxstWaAEiRNzGhirBPCk-1m0';
+  
 
 const sections = [
   {
